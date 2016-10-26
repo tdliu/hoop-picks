ee3dd2e9cf2dba92aaf4fe98b52d219a5296cf5d
Tony Liu <tliu.auckland@gmail.com>
2016-10-25 21:16:37 -0700

    removed redundant code from hoop_picks.py

--- hoop_picks.py
+++ hoop_picks.py
@@ -188,32 +188,6 @@ class PickHandler(webapp2.RequestHandler):
         data = json.loads(self.request.body)
         self.response.out.write(json.dumps(data))
 
-'''
-# [START guestbook]
-class Guestbook(webapp2.RequestHandler):
-
-    def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each
-        # Greeting is in the same entity group. Queries across the
-        # single entity group will be consistent. However, the write
-        # rate to a single entity group should be limited to
-        # ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
-
-        greeting.content = self.request.get('content')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
-# [END guestbook]
-'''
 
 # [START app]
 app = webapp2.WSGIApplication([
